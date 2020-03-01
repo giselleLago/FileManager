fc147edfa7dece310890ce2790600db07edef319
giselle <glj@holacons.com>
2020-03-01 02:12:42 +0100

    Craete new tests to JsonStudentDao.GetAll().

---
+++ FileManager.DataAccess.DataTests/JsonStudentDaoTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace FileManager.DataAccess.Data.Tests
+{
+    [TestClass()]
+    public class JsonStudentDaoTests
+    {
+        private const string FileName = "students.json";
+
+        [TestMethod()]
+        public void GetAll_GivenNonExistentFile_ShouldReturnAnEmptyList()
+        {
+            if (File.Exists(FileName))
+            {
+                File.Delete(FileName);
+            }
+            var jsonStudentDao = new JsonStudentDao();
+            var list = jsonStudentDao.GetAll();
+            Assert.AreEqual(0, list.Count);
+        }
+
+        [TestMethod()]
+        public void GetAll_GivenOneStudentInFile_ShouldReturnAListWithOneElement() 
+        {
+            var json = "[{ \"Name\":\"Pepe\", \"LastName\":\"Perez\", \"Age\": 20}]";
+            File.WriteAllText(FileName, json);
+            var jsonStudentDao = new JsonStudentDao();
+            var list = jsonStudentDao.GetAll();
+            Assert.AreEqual(1, list.Count);
+        }
+    }
+}
