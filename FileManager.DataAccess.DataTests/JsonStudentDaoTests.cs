a3bafcaff23e41da8e123465f6fdaedfc0604745
giselle <glj@holacons.com>
2020-03-01 17:18:59 +0100

    Create Integration test (CRUD) to XmlStudentDao.

--- FileManager.DataAccess.DataTests/JsonStudentDaoTests.cs
+++ FileManager.DataAccess.DataTests/JsonStudentDaoTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using FileManager.Common.Layer;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.IO;
 
 namespace FileManager.DataAccess.Data.Tests
@@ -21,13 +22,81 @@ namespace FileManager.DataAccess.Data.Tests
         }
 
         [TestMethod()]
-        public void GetAll_GivenOneStudentInFile_ShouldReturnAListWithOneElement() 
+        public void GetAll_GivenOneStudentInFile_ShouldReturnAListWithOneElement()
         {
             var json = "[{ \"Name\":\"Pepe\", \"LastName\":\"Perez\", \"Age\": 20}]";
             File.WriteAllText(FileName, json);
             var jsonStudentDao = new JsonStudentDao();
             var list = jsonStudentDao.GetAll();
             Assert.AreEqual(1, list.Count);
+        }
+
+        [TestMethod()]
+        public void Create_GivenAnEmptyFile_ShouldReturnAListWithOneElement()
+        {
+            if (File.Exists(FileName))
+            {
+                File.Delete(FileName);
+            }
+            var student = new Student
+            {
+                Name = "Pepe",
+                LastName = "Perez",
+                Age = 21
+            };
+            var jsonStudentDao = new JsonStudentDao();
+            jsonStudentDao.Create(student);
+            var jsonList = jsonStudentDao.GetAll();
+            Assert.AreEqual(1, jsonList.Count);
+        }
+        [TestMethod()]
+        public void Create_GivenOneStudentInFile_ShouldReturnAListWithTwoElements()
+        {
+            var json = "[{ \"Name\":\"Pepe\", \"LastName\":\"Perez\", \"Age\": 20}]";
+            File.WriteAllText(FileName, json);
+            var student = new Student
+            {
+                Name = "Pepe",
+                LastName = "Perez",
+                Age = 21
+            };
+            var jsonStudentDao = new JsonStudentDao();
+            jsonStudentDao.Create(student);
+            var jsonList = jsonStudentDao.GetAll();
+            Assert.AreEqual(2, jsonList.Count);
+        }
+        [TestMethod()]
+        public void Update_GivenOneStudentInFile_ShouldReturnAStudentWithDiferentNameAndAge()
+        {
+            var json = "[{ \"Id\":\"1\", \"Name\":\"Pepe\", \"LastName\":\"Perez\", \"Age\": 20}]";
+            File.WriteAllText(FileName, json);
+            var student = new Student
+            {
+                Id = "1",
+                Name = "Max",
+                LastName = "Perez",
+                Age = 21
+            };
+            var jsonStudentDao = new JsonStudentDao();
+            var updateStudent = jsonStudentDao.Update(student);
+            jsonStudentDao.GetAll();
+            Assert.AreEqual(updateStudent.Name, student.Name);
+            Assert.AreEqual(updateStudent.LastName, student.LastName);
+            Assert.AreEqual(updateStudent.Age, student.Age);
+        }
+        [TestMethod()]
+        public void Delete_GivenOneStudentInFile_ShouldReturnAnEmptyList()
+        {
+            var json = "[{ \"Id\":\"1\", \"Name\":\"Pepe\", \"LastName\":\"Perez\", \"Age\": 20}]";
+            File.WriteAllText(FileName, json);
+            var student = new Student
+            {
+                Id = "1"
+            };
+            var jsonStudentDao = new JsonStudentDao();
+            jsonStudentDao.Delete(student);
+            var jsonList = jsonStudentDao.GetAll();
+            Assert.AreEqual(0, jsonList.Count);
         }
     }
 }
---
+++ FileManager.DataAccess.DataTests/XmlStudentDaoTests.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.IO;
+using FileManager.Common.Layer;
+
+namespace FileManager.DataAccess.Data.Tests
+{
+    [TestClass()]
+    public class XmlStudentDaoTests
+    {
+        private const string FileName = "students.xml";
+
+        [TestMethod()]
+        public void GetAll_GivenNonExistentFile_ShouldReturnAnEmptyList()
+        {
+            if (File.Exists(FileName))
+            {
+                File.Delete(FileName);
+            }
+            var xmlStudentDao = new XmlStudentDao();
+            var list = xmlStudentDao.GetAll();
+            Assert.AreEqual(0, list.Count);
+        }
+
+        [TestMethod()]
+        public void GetAll_GivenOneStudentInFile_ShouldReturnAListWithOneElement()
+        {
+            var xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?><ArrayOfStudent xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">  <Student>    <Id>1</Id>    <Name>Peter</Name>    <LastName>Parker</LastName>    <Age>22</Age>  </Student></ArrayOfStudent>";
+            File.WriteAllText(FileName, xml);
+            var xmlStudentDao = new XmlStudentDao();
+            var list = xmlStudentDao.GetAll();
+            Assert.AreEqual(1, list.Count);
+        }
+
+        [TestMethod()]
+        public void Create_GivenAnEmptyFile_ShouldReturnAListWithOneElement()
+        {
+            if (File.Exists(FileName))
+            {
+                File.Delete(FileName);
+            }
+            var student = new Student
+            {
+                Name = "Pepe",
+                LastName = "Perez",
+                Age = 21
+            };
+            var xmlStudentDao = new XmlStudentDao();
+            xmlStudentDao.Create(student);
+            var list = xmlStudentDao.GetAll();
+            Assert.AreEqual(1, list.Count);
+        }
+
+        [TestMethod()]
+        public void Delete_GivenOneStudentInFile_ShouldReturnAnEmptyList()
+        {
+            var xml = "<?xml version=\"1.0\" encoding=\"utf-8\"?><ArrayOfStudent xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">  <Student>    <Id>1</Id>    <Name>Peter</Name>    <LastName>Parker</LastName>    <Age>22</Age>  </Student></ArrayOfStudent>";
+            File.WriteAllText(FileName, xml);
+            var student = new Student
+            {
+                Id = "1"
+            };
+            var xmlStudentDao = new XmlStudentDao();
+            xmlStudentDao.Delete(student);
+            var list = xmlStudentDao.GetAll();
+            Assert.AreEqual(0, list.Count);
+        }
+    }
+}
