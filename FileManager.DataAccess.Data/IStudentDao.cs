66cb200e1e909268fe6a98ec86b3319e959e3ae3
giselle <glj@holacons.com>
2020-03-01 02:11:50 +0100

    Create IStudentDao Interface.

---
+++ FileManager.DataAccess.Data/IStudentDao.cs
@@ -0,0 +1,13 @@
+﻿using FileManager.Common.Layer;
+using System.Collections.Generic;
+
+namespace FileManager.DataAccess.Data
+{
+    public interface IStudentDao
+    {
+        List<Student> GetAll();
+        Student Create(Student student);
+        Student Update(Student studentUpdate);
+        void Delete(Student studentDelete);
+    }
+}
