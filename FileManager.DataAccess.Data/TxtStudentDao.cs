6c7d7bf842e4f2b353d9d7ead1714af9e9241893
giselle <glj@holacons.com>
2020-03-03 20:39:14 +0100

    Create TxtStudentDao and unit tests.

---
+++ FileManager.DataAccess.Data/TxtStudentDao.cs
@@ -0,0 +1,86 @@
+﻿using FileManager.Common.Layer;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace FileManager.DataAccess.Data
+{
+    public class TxtStudentDao
+    {
+        private const string FileName = "students.txt";
+        public List<Student> GetAll()
+        {
+            if (File.Exists(FileName))
+            {
+                return DeserializeObject();
+            }
+
+            return new List<Student>();
+        }
+
+        public Student Create(Student student)
+        {
+            var studentList = GetAll();
+            studentList.Add(student);
+            SerializeObject(studentList);
+            return student;
+        }
+
+        public Student Update(Student studentUpdate)
+        {
+            var studentList = GetAll();
+            var student = studentList.FirstOrDefault(x => x.Id == studentUpdate.Id);
+            if (student == null)
+            {
+                throw new StudentNotFoundException();
+            }
+            student.Name = studentUpdate.Name;
+            student.LastName = studentUpdate.LastName;
+            student.Age = studentUpdate.Age;
+            SerializeObject(studentList);
+            return studentUpdate;
+        }
+
+        public void Delete(Student studentDelete)
+        {
+            var studentList = GetAll();
+            var student = studentList.FirstOrDefault(x => x.Id == studentDelete.Id);
+            if (student == null)
+            {
+                throw new StudentNotFoundException();
+            }
+            studentList.Remove(student);
+            SerializeObject(studentList);
+        }
+
+        private void SerializeObject(List<Student> studentList)
+        {
+            using (var tw = new StreamWriter(FileName))
+            {
+                foreach (var item in studentList)
+                {
+                    tw.WriteLine(string.Format("{0},{1},{2},{3}", item.Id, item.Name, item.LastName, item.Age));
+                }
+            }
+        }
+        private List<Student> DeserializeObject()
+        {
+            var studentList = new List<Student>();
+            var logFile = File.ReadLines(FileName).ToList();
+            for (int i = 0; i < logFile.Count; i++)
+            {
+                var line = logFile.ElementAt(i);
+                var data = line.Split(',');
+                var student = new Student
+                {
+                    Id = int.Parse(data[0]),
+                    Name = data[1],
+                    LastName = data[2],
+                    Age = int.Parse(data[3])
+                };
+                studentList.Add(student);
+            }
+            return studentList;
+        }
+    }
+}
--- FileManager.DataAccess.DataTests/TxtStudentDaoTests.cs
+++ FileManager.DataAccess.DataTests/TxtStudentDaoTests.cs
@@ -5,18 +5,18 @@ using FileManager.Common.Layer;
 namespace FileManager.DataAccess.Data.Tests
 {
     [TestClass()]
-    public class XmlStudentDaoTests
+    public class TxtStudentDaoTests
     {
-        private const string FileName = "students.xml";
-        
+        private const string FileName = "students.txt";
+
         [TestInitialize]
         public void Setup()
         {
-            var xmlStudentDao = new XmlStudentDao();
-            xmlStudentDao.Create(new Student { Id = 1, Name = "ParaTestear", LastName = "safsdf", Age = 6 });
-            xmlStudentDao.Create(new Student { Id = 2, Name = "ParaTestear", LastName = "safsdf", Age = 6 });
-            xmlStudentDao.Create(new Student { Id = 3, Name = "ParaTestear", LastName = "safsdf", Age = 6 });
-            xmlStudentDao.Create(new Student { Id = 4, Name = "ParaTestear", LastName = "safsdf", Age = 6 });
+            var txtStudentDao = new TxtStudentDao();
+            txtStudentDao.Create(new Student { Id = 1, Name = "ParaTestear", LastName = "safsdf", Age = 6 });
+            txtStudentDao.Create(new Student { Id = 2, Name = "ParaTestear", LastName = "safsdf", Age = 6 });
+            txtStudentDao.Create(new Student { Id = 3, Name = "ParaTestear", LastName = "safsdf", Age = 6 });
+            txtStudentDao.Create(new Student { Id = 4, Name = "ParaTestear", LastName = "safsdf", Age = 6 });
         }
 
         [TestCleanup]
@@ -32,37 +32,37 @@ namespace FileManager.DataAccess.Data.Tests
             {
                 File.Delete(FileName);
             }
-            var xmlStudentDao = new XmlStudentDao();
-            var list = xmlStudentDao.GetAll();
+            var txtStudentDao = new TxtStudentDao();
+            var list = txtStudentDao.GetAll();
             Assert.AreEqual(0, list.Count);
         }
 
         [TestMethod()]
-        public void Create_GivenAFourStudentsInFile_ShouldReturnAListWithFiveElement()
+        public void Create_GivenOneStudentInFile_ShouldReturnAListWithOneElement()
         {
             var student = new Student
             {
-                Id = 5,
+                Id = 7,
                 Name = "Pepe",
                 LastName = "Perez",
                 Age = 21
             };
-            var xmlStudentDao = new XmlStudentDao();
-            xmlStudentDao.Create(student);
-            var list = xmlStudentDao.GetAll();
+            var txtStudentDao = new TxtStudentDao();
+            txtStudentDao.Create(student);
+            var list = txtStudentDao.GetAll();
             Assert.AreEqual(5, list.Count);
         }
 
         [TestMethod()]
-        public void Update_GivenFourStudentsInFile_ShouldReturnAListWithSixElement()
+        public void Update_GivenAnEmptyFile_ShouldReturnAListWithOneElement()
         {
             var student = new Student
             {
-                Id = 1
+                Id = 2
             };
-            var xmlStudentDao = new XmlStudentDao();
-            xmlStudentDao.Update(student);
-            var list = xmlStudentDao.GetAll();
+            var txtStudentDao = new TxtStudentDao();
+            txtStudentDao.Update(student);
+            var list = txtStudentDao.GetAll();
             Assert.AreEqual(4, list.Count);
         }
 
@@ -73,11 +73,12 @@ namespace FileManager.DataAccess.Data.Tests
             {
                 Id = 1
             };
-            var xmlStudentDao = new XmlStudentDao();
-            xmlStudentDao.Delete(student);
-            var list = xmlStudentDao.GetAll();
+            var txtStudentDao = new TxtStudentDao();
+            txtStudentDao.Delete(student);
+            var list = txtStudentDao.GetAll();
             Assert.AreEqual(3, list.Count);
         }
+
         [ClassCleanup]
         public static void ClassCleanUp()
         {
